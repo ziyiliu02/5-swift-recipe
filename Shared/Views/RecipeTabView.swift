88baa49bdd9c5f94b27b9b10bd236d24d8998bae
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-24 15:38:17 +0800

    - Add RecipeTabView as the starting point for the app

---
+++ Shared/Views/RecipeTabView.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeTabView.swift
+//  5-swift-recipe (iOS)
+//
+//  Created by Liu Ziyi on 24/5/23.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+            
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
--- Shared/__swift_recipeApp.swift
+++ Shared/__swift_recipeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct __swift_recipeApp: App {
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            RecipeTabView()
         }
     }
 }
