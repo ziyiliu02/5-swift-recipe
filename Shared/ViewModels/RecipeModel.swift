e9664e37ba93f352b8ef34a9a0f4ecba12211c55
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-24 13:30:47 +0800

    - Create an instance of data service in RecipeModel and get the data

--- Shared/ViewModels/RecipeModel.swift
+++ Shared/ViewModels/RecipeModel.swift
@@ -11,4 +11,10 @@ class RecipeModel: ObservableObject {
  
     @Published var recipes = [Recipe]()
     
+    init() {
+        // Create an instance of data service and get the data
+        let service = DataService()
+        self.recipes = service.getLocalData()
+    }
+    
 }
