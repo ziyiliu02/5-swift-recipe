3a87254c13f0bcc5d9a244c86c7bbc638b4d35d5
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-26 12:48:41 +0800

    - Include serving size picker in RecipeDetailView

--- Shared/Views/RecipeDetailView.swift
+++ Shared/Views/RecipeDetailView.swift
@@ -12,6 +12,8 @@ struct RecipeDetailView: View {
     // The RecipeDetailView relies on having a reference to a single recipe because it' going to display the detail for that recipe. So going to declare a property here and it's going to be the data type Recipe. Not going to set this property to any particular instance because i'm going to rely on the list view to assign the particular recipe that we want to use to display the UI.
     var recipe:Recipe
     
+    @State var selectedServingSize = 0
+    
     var body: some View {
         ScrollView {
             VStack(alignment:.leading){
@@ -19,6 +21,20 @@ struct RecipeDetailView: View {
                 Image(recipe.image)
                     .resizable()
                     .scaledToFill()
+                
+                //MARK: Serving Size Picker
+                VStack(alignment:.leading){
+                    Text("Select your serving size:")
+                    Picker("", selection: $selectedServingSize){
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width:160)
+                }
+                .padding()
                 
                 //MARK: Ingredients
                 VStack(alignment:.leading){
@@ -29,7 +45,7 @@ struct RecipeDetailView: View {
                     ForEach(recipe.ingredients, id:\.self){ item in
                         Text("- " + item)
                     }
-                }
+                }.padding(.horizontal)
                 
                 //MARK: Divider
                 Divider()
